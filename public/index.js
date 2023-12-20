f3b35fe532d8c76353ae62a0027eb8d511a9ce00
Ian Bailey <IanKulin@kulin.com.au>
2023-12-20 15:59:11 +0800

    refactor to pull out duplicate code

--- public/index.js
+++ public/index.js
@@ -1,37 +1,47 @@
 // index.js - code for simple todo app
 // json data served from node endpoint
 
-// Get todos from node endpoint
+// Function to create a todo item
+function createTodoItem(todoText, id) {
+    const li = document.createElement('li');
+    const button = document.createElement('button');
+    button.innerHTML = 'Done';
+    button.addEventListener('click', () => handleDelete(id, li));
+
+    // Create a text node with the todo text and append it to the li
+    const todoTextNode = document.createTextNode(todoText);
+    li.appendChild(todoTextNode);
+
+    // Then append the delete button
+    li.appendChild(button);
+
+    return li;
+}
+
+
+function handleDelete(id, li) {
+    fetch('/todos/' + id, {
+        method: 'DELETE'
+    })
+        .then(res => res.json())
+        .then(() => {
+            li.remove();
+        });
+}
+
+// Fetch the todo items to build the list
 fetch('/todos')
     .then(res => res.json())
     .then(todos => {
         // Loop through todos and add to list
         todos.forEach(todo => {
-            const li = document.createElement('li');
-            const button = document.createElement('button');
-            button.innerHTML = 'Done';
-            button.addEventListener('click', () => {
-                console.log('clicked')
-                fetch('/todos/' + todo.id, {
-                    method: 'DELETE'
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        li.remove();
-                    });
-            });
-
-            // Create a text node with the todo text and append it to the li
-            const todoText = document.createTextNode(todo.todo);
-            li.appendChild(todoText);
-
-            // Then append the delete button
-            li.appendChild(button);
-
+            const li = createTodoItem(todo.todo, todo.id);
             document.querySelector('#todos').appendChild(li);
         });
     });
-// Add a todo
+
+
+// handler for adding a todo item
 document.querySelector('form').addEventListener('submit', (e) => {
     e.preventDefault();
     const todo = document.querySelector('#todo').value;
@@ -44,28 +54,8 @@ document.querySelector('form').addEventListener('submit', (e) => {
     })
         .then(res => res.json())
         .then(data => {
-            const li = document.createElement('li');
-            const button = document.createElement('button');
-            button.innerHTML = 'Done';
-            button.addEventListener('click', () => {
-                console.log('clicked')
-                fetch('/todos/' + data.id, {
-                    method: 'DELETE'
-                })
-                    .then(res => res.json())
-                    .then(data => {
-                        li.remove();
-                    });
-            });
-
-            // Create a text node with the todo text and append it to the li
-            const todoText = document.createTextNode(todo);
-            li.appendChild(todoText);
-
-            // Then append the delete button
-            li.appendChild(button);
-
+            const li = createTodoItem(todo, data.id);
             document.querySelector('#todos').appendChild(li);
             document.querySelector('#todo').value = '';
         });
-}); 
+});
