ddbcf6e19a3326fee971f241d263abf1e217c370
Ian Bailey <IanKulin@kulin.com.au>
2024-01-21 19:28:21 +0800

    styling

--- src/Login.jsx
+++ src/Login.jsx
@@ -2,6 +2,7 @@
 import { useForm } from "react-hook-form";
 import { useState } from "react";
 import pb from "../services/pocketbase.js";
+import "./styles.css";
 
 const Login = ({ onLogin }) => {
   const { register, handleSubmit } = useForm();
@@ -10,30 +11,39 @@ const Login = ({ onLogin }) => {
 
   async function login(data) {
     try {
-      await pb
-        .collection("users")
-        .authWithPassword(data.email, data.password);
-        onLogin();
+      await pb.collection("users").authWithPassword(data.email, data.password);
+      onLogin();
     } catch (error) {
       setLoading(false);
       setHasFailed(true);
     }
   }
 
-
   return (
-    <div>
+    <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
       {isLoading && <p>Loading...</p>}
       {hasFailed && <p>Failed to authenticate, try again</p>}
-      <form onSubmit={handleSubmit(login)}>
-        <label>
+      <form onSubmit={handleSubmit(login)} style={{ display: "block" }}>
+        <label style={{ display: "block" }}>
           Email:
-          <input type="text" name="email" {...register("email")} />
+          <input
+            type="text"
+            name="email"
+            {...register("email")}
+            style={{ display: "block" }}
+          />
         </label>
-        <label>
+        <br />
+        <label style={{ display: "block" }}>
           Password:
-          <input type="password" name="password" {...register("password")} />
-        </label>
+          <input
+            type="password"
+            name="password"
+            {...register("password")}
+            style={{ display: "block" }}
+          />
+        </label>{" "}
+        <br />
         <input type="submit" disabled={isLoading} value="Submit" />
       </form>
     </div>
